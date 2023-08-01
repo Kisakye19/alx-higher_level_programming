f87f77bed14793b020ef04d12d1f17d7501c4a7c
Terry Wambui <shikuwambui@gmail.com>
2023-08-01 22:20:42 +0300

    Perfection requires hard work

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,26 +1,18 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_height - calculate height of a binary tree
- * @tree: pointer to the root node
- * Return: 0 on failure || height of the tree
+ * binary_tree_is_perfect - A function that checks if binary tree is perfect.
+ * @tree: A pointer to the root of the tree.
+ * Return: 1 if binary tree is perfect or 0 in otherwise.
  */
-
-size_t binary_tree_height(const binary_tree_t *tree)
+int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t leftheight = 0;
-	size_t rightheight = 0;
-
-	if (tree == NULL)
+	if (!tree)
 		return (0);
-
-	leftheight += 1 + binary_tree_height(tree->left);
-	rightheight += 1 + binary_tree_height(tree->right);
-
-	if (leftheight > rightheight)
-		return (leftheight);
-	else
-		return (rightheight);
+	if ((binary_tree_is_full(tree) == 1) &&
+			(binary_tree_balance(tree) == 0))
+		return (1);
+	return (0);
 }
 
 /**
@@ -70,21 +62,24 @@ int binary_tree_is_full(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_is_perfect - checks if a binary tree is perfect
+ * binary_tree_height - calculate height of a binary tree
  * @tree: pointer to the root node
- * is perfect if all internal nodes have 2 children & all leaves are at the
- * same level
- *
- * Return: 0 if tree is NULL
+ * Return: 0 on failure || height of the tree
  */
 
-int binary_tree_is_perfect(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t leftheight = 0;
+	size_t rightheight = 0;
+
 	if (tree == NULL)
 		return (0);
 
-	if ((binary_tree_is_full(tree) == 1) &&
-			(binary_tree_balance(tree) == 0))
-		return (1);
-	return (0);
+	leftheight += 1 + binary_tree_height(tree->left);
+	rightheight += 1 + binary_tree_height(tree->right);
+
+	if (leftheight > rightheight)
+		return (leftheight);
+	else
+		return (rightheight);
 }
