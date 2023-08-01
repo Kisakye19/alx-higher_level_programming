acb7ca6a6278634fbfd1e1912a538fa7a2fcdaf4
Terry Wambui <shikuwambui@gmail.com>
2023-08-01 22:03:54 +0300

    Perfection requires hard work

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -23,6 +23,24 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (rightheight);
 }
 
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: pointer to the root node
+ * Return: 0 if tree is NULL
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t leftheight, rightheight;
+
+	if (tree == NULL)
+		return (0);
+
+	leftheight = binary_tree_height(tree->left);
+	rightheight = binary_tree_height(tree->right);
+
+	return (leftheight - rightheight);
+}
 
 /**
  * binary_tree_is_full - checks if a binary tree is full
@@ -62,16 +80,11 @@ int binary_tree_is_full(const binary_tree_t *tree)
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
-	size_t leftheight, rightheight;
-
 	if (tree == NULL)
 		return (0);
 
-	leftheight = binary_tree_height(tree->left);
-	rightheight = binary_tree_height(tree->right);
-
-	if (leftheight != rightheight)
-		return (0);
-
-	return (binary_tree_is_full(tree));
+	if ((binary_tree_is_full(tree) == 1) &&
+			(binary_tree_balance(tree) == 0))
+		return (1);
+	return (0);
 }
