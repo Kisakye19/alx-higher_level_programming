674a9805c87148d6b764ae5de3bcbfbdcdbc4fbf
Terry Wambui <shikuwambui@gmail.com>
2023-08-02 01:13:17 +0300

    find the sibling

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -8,15 +8,11 @@
 
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
-	binary_tree_t *parent = node->parent;
-
 	if (node == NULL || node->parent == NULL)
 		return (NULL);
 
-	if (parent->left == node)
-		return (parent->right);
-	else if (parent->right == node)
-		return (parent->left);
+	if (node->parent->left == node)
+		return (node->parent->right);
 	else
-		return (NULL);
+		return (node->parent->left);
 }
